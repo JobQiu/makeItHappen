d16237a4d82164896331299e6a4061eb5d6f7d01
Congmin <congminqi@gmail.com>
2018-06-07 11:50:46 -0500

    Initial Commit

---
+++ makeItHappen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  makeItHappen
+//
+//  Created by CongminQiu on 6/7/18.
+//  Copyright © 2018 CongminQiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
